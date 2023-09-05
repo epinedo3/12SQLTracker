8faaf78e6481a439567fd00e0900c9e38426b362
Enrique Pinedo <epinedo31@outlook.com>
2023-09-04 20:22:43 -0500

    finished operations folder

--- operations/addEmployee.js
+++ operations/addEmployee.js
@@ -1,4 +1,16 @@
+const { printTable } = require("console-table-printer")
+const inquirer = require("inquirer")
 function addAnEmployee (connection, startApp) {
+    inquirer.prompt([{
+        type: "input",
+        message: "enter new employee",
+        name: "employee"
+    }])
+        .then(answer => {
+            const statement = connection.query("insert into employee (name) values(?)",[answer.employee], (err, data) => {
+                startApp()
+            })
+        })
 
 }
 module.exports = addAnEmployee;
--- operations/addRole.js
+++ operations/addRole.js
@@ -1,4 +1,15 @@
+const { printTable } = require("console-table-printer")
+const inquirer = require("inquirer")
 function addARole (connection, startApp){
-
+    inquirer.prompt([{
+        type: "input",
+        message: "enter new role",
+        name: "role"
+    }])
+        .then(answer => {
+            const statement = connection.query("insert into role (name) values(?)",[answer.role], (err, data) => {
+                startApp()
+            })
+        })
 }
 module.exports = addARole;
